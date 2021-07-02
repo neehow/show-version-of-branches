94bef1c77f30265b2ed85e28f7927fc6327a77a5
wsq <wsq@wsqdeMacBook-Pro.local>
2021-07-02 11:00:12 +0800

    remove reverse option, add comment

--- show-version-of-branches.sh
+++ show-version-of-branches.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+# TODO: there's a problem sorting results
 echo "----"
 for branch in $(git branch -a | grep "remotes" | grep -v ">");
   do git show $branch:package.json | grep \"version\" | xargs echo -n && echo " "$branch;
-done | sort -rn
+done | sort -n
 echo "----"
